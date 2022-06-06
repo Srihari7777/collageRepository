a0760832f11a49d417fceee3d3118e859f736d8f
Srihari Kumbha <srihari.kumbha@ibm.com>
2022-06-06 11:23:47 +0530

    he is from north india to south

---
+++ TeacherStudent.java
@@ -0,0 +1,25 @@
+public class TeacherStudent
+
+	public void Teacher(){
+		System.out.println("the best teacher award goes to mr.pankaj");
+		System.out.println("he is from north india");
+
+
+         }
+	public void Student(){
+		System.out.println("the best student is ramcharan");
+		System.out.println("he is from north india");
+
+
+
+	}
+
+	public static void main(String args[])
+	{
+
+		TeacherStudent t=new TeacherStudent();
+		t.Teacher();
+		t.Student();
+
+
+	} 
