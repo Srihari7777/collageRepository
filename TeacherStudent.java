2aa1b0de5e0b4c62830afeeef44d57a82d4f9b37
Srihari Kumbha <srihari.kumbha@ibm.com>
2022-06-06 11:35:44 +0530

    some changes done

--- TeacherStudent.java
+++ TeacherStudent.java
@@ -2,13 +2,15 @@ public class TeacherStudent
 
 	public void Teacher(){
 		System.out.println("the best teacher award goes to mr.pankaj");
-		System.out.println("he is from north india");
+		System.out.println("he is from IBM")
+		
+		
 
 
          }
 	public void Student(){
 		System.out.println("the best student is ramcharan");
-		System.out.println("he is from north india");
+		System.out.println("he is from IBM")
 
 
 
